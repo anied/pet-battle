19d1dbb90a151e919d4b195a1e547227d9151c83
Alexander Nied <alex.nied@spr.com>
2023-10-30 15:29:22 -0500

    Gets more meaningful logging into shell file and breaks to lines

--- shell/run_docker.sh
+++ shell/run_docker.sh
@@ -1,1 +1,16 @@
-docker start pg-pet-battle || docker run  --env-file ./env/pg-docker.env -p 5432:5432 --runtime=runc --name=pg-pet-battle -v postgres-data:/var/lib/postgresql/data -d postgres:alpine3.18
+(echo "Attempting to start docker container pg-pet-battle..." \
+&& \
+docker start pg-pet-battle \
+&& \
+echo "pg-pet-battle container running!") \
+|| (echo "pg-pet-battle container not found; creating new container from image..." \
+&& \
+docker run  \
+--env-file ./env/pg-docker.env \
+-p 5432:5432 \
+--runtime=runc \
+--name=pg-pet-battle \
+-v postgres-data:/var/lib/postgresql/data \
+-d postgres:alpine3.18 \
+&& \
+echo "pg-pet-battle container created successfully and running!")
