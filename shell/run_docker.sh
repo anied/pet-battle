3c48d25bb0614faf777ffb61c41f28471061f41c
Alexander Nied <alex.nied@spr.com>
2023-10-30 15:04:35 -0500

    Updates script to first attempt to start if container exists

--- shell/run_docker.sh
+++ shell/run_docker.sh
@@ -1,13 +1,1 @@
-# echo "Running docker container for postgres"
-
-# echo "logging location of pg-docker"
-# ls ./env
-
-# echo "logging contents of file"
-# cat ./env/pg-docker.env
-
-# # echo "Creating docker volume for postgres"
-# # --volume=/var/lib/postgresql/data
-
-echo "Creating docker container for postgres"
-docker run  --env-file ./env/pg-docker.env -p 5432:5432 --runtime=runc --name=pg-pet-battle -v postgres-data:/var/lib/postgresql/data -d postgres:alpine3.18
+docker start pg-pet-battle || docker run  --env-file ./env/pg-docker.env -p 5432:5432 --runtime=runc --name=pg-pet-battle -v postgres-data:/var/lib/postgresql/data -d postgres:alpine3.18
