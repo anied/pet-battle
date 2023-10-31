c1f5fa59973bdb867248490f1b3c2d6925e8192f
Alexander Nied <alex.nied@spr.com>
2023-10-31 12:09:18 -0500

    WIP: Gets to making create DB request but SQL syntax is incorrect

--- db_scripts/create_petbattle_db.js
+++ db_scripts/create_petbattle_db.js
@@ -1,7 +1,9 @@
-import { pgAdmin } from 'db/postgres';
+/* eslint-disable @typescript-eslint/no-var-requires */
+const pgAdmin = require('../db/postgres_admin');
 
 // Based on a function provided by ChatGPT
-async function createDatabaseIfNotExists(databaseName?: string) {
+async function createDatabaseIfNotExists(databaseName) {
+  console.log('entered createDatabaseIfNotExists');
   if (!databaseName) {
     console.error(
       `No database name provided; databaseName passed as ${databaseName}.  Database not created; exiting...`,
@@ -10,6 +12,9 @@ async function createDatabaseIfNotExists(databaseName?: string) {
   }
   try {
     // Connect to the default 'postgres' database to check if the target database exists
+    console.log(
+      `Attempting to create or connect to database "${databaseName}"...`,
+    );
     await pgAdmin.query(`CREATE DATABASE IF NOT EXISTS "${databaseName}";`);
     console.log(
       `Database "${databaseName}" has been created or already exists.`,
@@ -21,6 +26,10 @@ async function createDatabaseIfNotExists(databaseName?: string) {
   }
 }
 
-const databaseName = process.env.PG_DATABASE;
-
+// async function createAppDb() {
+console.log('entered createPetbattleDb');
+const databaseName = process.env.APP_DB_NAME;
 createDatabaseIfNotExists(databaseName);
+// }
+
+// module.exports = createAppDb;
